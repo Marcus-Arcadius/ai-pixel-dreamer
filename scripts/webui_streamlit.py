282040e5c322abfc1efea7b0fa55907656f14ebf
Werner Oswald <osiworx@gmail.com>
2022-09-24 13:44:53 +0200

    some updates and new blocks

--- scripts/webui_streamlit.py
+++ scripts/webui_streamlit.py
@@ -5,7 +5,7 @@ import importlib
 # import streamlit_nested_layout
 
 # streamlit components section
-from st_on_hover_tabs import on_hover_tabs
+#from st_on_hover_tabs import on_hover_tabs
 
 # other imports
 import os
@@ -44,7 +44,7 @@ os.environ["CUDA_VISIBLE_DEVICES"] = str(st.session_state["defaults"].general.gp
 
 
 # functions to load css locally OR remotely starts here. Options exist for future flexibility. Called as st.markdown with unsafe_allow_html as css injection
-# TODO, maybe look into async loading the file especially for remote fetching 
+# TODO, maybe look into async loading the file especially for remote fetching
 def local_css(file_name):
 	with open(file_name) as f:
 		st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
@@ -62,13 +62,13 @@ def load_css(isLocal, nameOrURL):
 
 
 def layout():
-	st.set_page_config(page_title="AI Pixel Dreamer", layout="wide", initial_sidebar_state="collapsed")
+	st.set_page_config(page_title="Stable Diffusion Playground", layout="wide", initial_sidebar_state="collapsed")
 
 	with st.empty():
 		# load css as an external file, function has an option to local or remote url. Potential use when running from cloud infra that might not have access to local path.
 		load_css(True, 'scripts/tools/css/streamlit.main.css')
 	# check if the models exist on their respective folders
-	if os.path.exists(os.path.join(defaults.general.GFPGAN_dir, "experiments", "pretrained_models", "GFPGANv1.3.pth")):
+	if os.path.exists(os.path.join(defaults.general.GFPGAN_dir, "GFPGANv1.3.pth")):
 		st.session_state["GFPGAN_available"] = True
 	else:
 		st.session_state["GFPGAN_available"] = False
@@ -79,16 +79,16 @@ def layout():
 	else:
 		st.session_state["RealESRGAN_available"] = False
 
-	with st.sidebar:
-		# we should use an expander and group things together when more options are added so the sidebar is not too messy.
-		# with st.expander("Global Settings:"):
-		st.write("Global Settings:")
-		defaults.general.update_preview = st.checkbox("Update Image Preview", value=defaults.general.update_preview,
-													  help="If enabled the image preview will be updated during the generation instead of at the end. You can use the Update Preview \
-							      Frequency option bellow to customize how frequent it's updated. By default this is enabled and the frequency is set to 1 step.")
-		defaults.general.update_preview_frequency = st.text_input("Update Image Preview Frequency",
-																  value=defaults.general.update_preview_frequency,
-																  help="Frequency in steps at which the the preview image is updated. By default the frequency is set to 1 step.")
+	#with st.sidebar:
+	# we should use an expander and group things together when more options are added so the sidebar is not too messy.
+	# with st.expander("Global Settings:"):
+	#	st.write("Global Settings:")
+	#	defaults.general.update_preview = st.checkbox("Update Image Preview", value=defaults.general.update_preview,
+	#												  help="If enabled the image preview will be updated during the generation instead of at the end. You can use the Update Preview \
+	#						      Frequency option bellow to customize how frequent it's updated. By default this is enabled and the frequency is set to 1 step.")
+	#	defaults.general.update_preview_frequency = st.text_input("Update Image Preview Frequency",
+	#															  value=defaults.general.update_preview_frequency,
+	#															  help="Frequency in steps at which the the preview image is updated. By default the frequency is set to 1 step.")
 
 	# txt2img_tab, img2img_tab, txt2video, postprocessing_tab = st.tabs(["Text-to-Image Unified", "Image-to-Image Unified", "Text-to-Video","Post-Processing"])
 	# scan plugins folder for plugins and add them to the st.tabs
